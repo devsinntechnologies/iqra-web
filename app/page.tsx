aa4fd5c06da1ec3ca788a68cc658338036f61a8b
Devsinn Technologies <81500860+devsinntechnologies@users.noreply.github.com>
2026-03-05 01:15:06 +0500

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
       {/* Side Dots Navigation */}
       <SideScrollNav />
       
+      
 <section id="hero" className="h-screen">
   <HeroSection />
 </section>
