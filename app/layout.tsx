df2985f74e76d5f8a6629578c327efcbe3e6d475
arham <148682737+Azan-Asim@users.noreply.github.com>
2026-04-25 20:00:50 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+       <script type="module" src="https://widget.chatsupplies.com/index.js" id="chatbot" data-admin-id="123"   api-key="lw5gxHjpSkDW9hx1Y1k5"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
